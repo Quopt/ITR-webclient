cb6be41a3e7022773980d47f5ea571b7074ec9b9
bgarssen <31415742+bgarssen@users.noreply.github.com>
2019-03-16 17:06:10 +0100

    Added copyright message
    And link to the inspirator for this script, Gabor Szabo

--- http_serve_folder.py
+++ http_serve_folder.py
@@ -1,3 +1,20 @@
+# Inspired by Gabor Szabo's example at https://code-maven.com/static-server-in-python
+#
+# Copyright 2019 by Quopt IT Services BV
+#
+#  Licensed under the Artistic License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    https://opensource.org/licenses/Artistic-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os, sys
 
@@ -42,7 +59,6 @@ class StaticServer(BaseHTTPRequestHandler):
  
     def do_GET(self):
         global root, content_type
-        #print(self.path)
         if self.path == '/':
             filename = root + os.sep +'default.htm'
         else:
@@ -64,7 +80,6 @@ class StaticServer(BaseHTTPRequestHandler):
         self.end_headers()
         with open(filename, 'rb') as fh:
             html = fh.read()
-            #html = bytes(html, 'utf8')
             self.wfile.write(html)
  
 def run(server_class=HTTPServer, handler_class=StaticServer, port=8000):
