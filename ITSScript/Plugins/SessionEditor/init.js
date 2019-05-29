8964f180afa27d82f29ed9280f6ef93fbf4bafb7
bgarssen@gmail.com <Hoi02Hoi!>
2019-05-29 20:03:15 +0200

    Fix for showing answers on paid tests within 24 hours

--- ITSScript/Plugins/SessionEditor/init.js
+++ ITSScript/Plugins/SessionEditor/init.js
@@ -439,7 +439,7 @@
             oneday.setHours(oneday.getHours() + 24);
             if ( (this.currentSession.SessionTests[testIndex].TestEnd < new Date(2001,1,1) ) ||
                  (this.currentSession.SessionTests[testIndex].testDefinition.Costs <= 0) ||
-                 (this.currentSession.SessionTests[testIndex].TestEnd > oneday) ) {
+                 (this.currentSession.SessionTests[testIndex].TestEnd < oneday) ) {
                 ITSInstance.SessionViewAnswersSessionController.currentSession = this.currentSession;
                 ITSRedirectPath('SessionViewAnswers&SessionID=' + this.currentSession.ID + '&SessionTestID=' + testSessionID + "&checkAnswers=1");
             } else {
