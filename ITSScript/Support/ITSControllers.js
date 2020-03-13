65e642602e246603c2770bc62c518c69ac02067a
bgarssen@gmail.com <Hoi02Hoi!>
2020-03-13 16:16:06 +0100

    Added 2 new messages in test taking, NextUnansweredScreen and NextUnansweredScreenWithProceed

--- ITSScript/Support/ITSControllers.js
+++ ITSScript/Support/ITSControllers.js
@@ -713,6 +713,19 @@ ITSTestTakingController.prototype.processEvent = function (eventName, eventParam
             this.saveCurrentTest();
             setTimeout(this.renderTestPage.bind(this),100);
             break;
+        case "NextUnansweredScreenWithProceed":
+            this.currentSessionTest.CurrentPage++;
+        case "NextUnansweredScreen" :
+            if (this.InTestTaking) $('#NavbarsTestTaking').show();
+            var oldPage = this.currentSessionTest.CurrentPage;
+            while ( (this.currentTestDefinition.screens[this.currentSessionTest.CurrentPage].show) &&
+                    (this.currentSessionTest.CurrentPage < this.currentTestDefinition.screens.length) &&
+                    (this.currentTestDefinition.screens[this.currentSessionTest.CurrentPage].checkIsAnswered( this.currentSessionTest.Results ) >= 1  ) ) {
+                this.currentSessionTest.CurrentPage++;
+            }
+            if (oldPage !=  this.currentSessionTest.CurrentPage) { this.saveCurrentTest();}
+            setTimeout(this.renderTestPage.bind(this),100);
+            break;
         case "PreviousScreen" :
      		if (this.InTestTaking) $('#NavbarsTestTaking').show();
             var oldCurrentPage = this.currentSessionTest.CurrentPage;
