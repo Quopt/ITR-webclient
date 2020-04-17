3b53cb6f6c9c547492871f4f3f8a537f52fc3fb3
bgarssen@gmail.com <Hoi02Hoi!>
2020-04-17 18:44:48 +0200

    Refinements to dark mode option to the office interface

--- ITSScript/Support/ITS_UI.js
+++ ITSScript/Support/ITS_UI.js
@@ -558,7 +558,6 @@ ITSUIController = function () {
     };
     this.afterOfficeLoginUI = function () {
         ITSInstance.MessageBus.subscribe("CurrentUser.Loaded", ITSInstance.UIController.setDarkModeToUserPreference, true ) ;
-        $('#ITRColorMode').unbind().change(ITSInstance.UIController.changeDarkMode);
     };
 }
 
