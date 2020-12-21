acbd41c10bfaef8e0fc6d49d0e1cf76391f8f9c3
bgarssen@hotmail.com <Hoi02Hoi!>
2020-12-21 13:30:01 +0100

    Fix for requirements.txt not installing correctly

--- ITSScript/Plugins/ServerSettingsPublics/init.js
+++ ITSScript/Plugins/ServerSettingsPublics/init.js
@@ -338,7 +338,7 @@
                 ITSInstance.genericAjaxUpdate('installpublics/itr-api', "{}",
                     function () {
                         // force update
-                        ITSInstance.genericAjaxUpdate('installpublics/itr-stop', "{}", function (){}, function (){});
+                        //ITSInstance.genericAjaxUpdate('installpublics/itr-stop', "{}", function (){}, function (){});
                         ITSInstance.UIController.showInterfaceAsWaitingOff();
                         location.reload(true);
                      }, function () { ITSInstance.ServerSettingsPublicsController.sourceCodeUpdateFailed(); });
